4583331c4fb85ee518244ffb35cfb61fde3109dc
JulianMalnero <jmalnero@fi.uba.ar>
2025-10-11 19:30:54 -0300

    Se implementan funciones en lista_enlazada.go

--- lista/lista_enlazada.go
+++ lista/lista_enlazada.go
@@ -38,7 +38,18 @@ func (l *listaEnlazada[T]) InsertarPrimero(dato T) {}
 
 func (l *listaEnlazada[T]) InsertarUltimo(dato T) {}
 
-func (l *listaEnlazada[T]) BorrarPrimero() T {}
+func (l *listaEnlazada[T]) BorrarPrimero() T {
+	if l.EstaVacia() {
+		panic("La lista esta vacia")
+	}
+	dato := l.primero.dato
+	l.primero = l.primero.siguiente
+	if l.primero == nil {
+		l.ultimo = nil
+	}
+	l.largo--
+	return dato
+}
 
 func (l *listaEnlazada[T]) VerPrimero() T {
 }
@@ -49,19 +60,55 @@ func (l *listaEnlazada[T]) Largo() int {
 	return l.largo
 }
 
-func (l *listaEnlazada[T]) Iterar(visitar func(T) bool) {}
+func (l *listaEnlazada[T]) Iterar(visitar func(T) bool) {
+	puntero := l.primero
+	for puntero != nil {
+		if !visitar(puntero.dato) {
+			return
+		}
+		puntero = puntero.siguiente
+	}
+}
 
-func (l *listaEnlazada[T]) Iterador() IteradorLista[T] {}
+func (l *listaEnlazada[T]) Iterador() IteradorLista[T] {
+	return &iterListaEnlazada[T]{
+		actual:   l.primero,
+		anterior: nil,
+		lista:    l,
+	}
+}
 
 // Metodos del iterador externo
 
 func (i *iterListaEnlazada[T]) VerActual() T {}
 
-func (i *iterListaEnlazada[T]) HaySiguiente() bool {}
+func (i *iterListaEnlazada[T]) HaySiguiente() bool {
+	return i.actual != nil
+}
 
 func (i *iterListaEnlazada[T]) Siguiente() {}
 
 func (i *iterListaEnlazada[T]) Insertar(nuevoElemento T) {}
 
-func (i *iterListaEnlazada[T]) Borrar() T {}
+func (i *iterListaEnlazada[T]) Borrar() T {
+	if !i.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+
+	borrado := i.actual.dato
+
+	if i.anterior == nil {
+		i.lista.primero = i.actual.siguiente
+	} else {
+		i.anterior.siguiente = i.actual.siguiente
+	}
+	if i.actual.siguiente == nil {
+		i.lista.ultimo = i.anterior
+	}
+
+	i.actual = i.actual.siguiente
+	i.lista.largo--
+
+	return borrado
+}
 
